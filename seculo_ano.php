6f0af576be199a0d35196e484308ad4597a7e490
Gilson Meira <gilsonmeira97@gmail.com>
2022-01-08 21:15:40 +0100

    Conclusão teste seculo/ano

---
+++ seculo_ano.php
@@ -0,0 +1,13 @@
+<?php 
+
+    function SeculoAno($ano) {
+        $seculos = $ano / 100;
+        if($ano % 100 == 0) {
+            return $seculos;
+        } else {
+            return ceil($seculos) ;
+        }
+    }
+
+    echo seculoAno(1705);
+?>
